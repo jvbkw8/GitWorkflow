de357561b04b15ac2914a3458405999fa368a74b
jvbkw8 <jvbkw8@mail.missouri.edu>
2017-09-28 13:41:57 -0500

    Update ViewController.swift

--- GitWorkflow/ViewController.swift
+++ GitWorkflow/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Awesome Programming
+    //Super Awesome Programming
 
     override func viewDidLoad() {
         super.viewDidLoad()
